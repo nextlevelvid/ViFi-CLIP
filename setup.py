a4584124ea4e78a90e66b6ceb0f4856538791fac
Benjamin Frost <bfrost.schule@gmail.com>
2024-11-11 17:01:28 +0100

    fix: mmcv package

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     install_requires=[
         "torch==1.11.0",
         "torchvision==0.12.0",
+        "mmcv==1.7.2",
         "pathlib",
-        "mmcv-full",
         "decord",
         "ftfy",
         "einops",
